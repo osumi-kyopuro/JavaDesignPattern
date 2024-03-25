2a42aa97bf1a3f75de9e1c476f197bfd14d386da
osumi-kyopuro <osumiyuki1123@gmail.com>
2024-03-26 00:45:30 +0900

    Adapterパターン追加

---
+++ AdapterInheritance/Print.java
@@ -0,0 +1,7 @@
+package AdapterInheritance;
+
+public interface Print {
+    public abstract void printWeak();
+
+    public abstract void printStrong();
+}
---
+++ AdapterInheritance/PrintBanner.java
@@ -0,0 +1,17 @@
+package AdapterInheritance;
+
+public class PrintBanner extends Banner
+        implements Print {
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    public void printWeak() {
+        showWithParen();
+    }
+
+    public void printStrong() {
+        showWithAster();
+    }
+}
